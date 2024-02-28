fbcbb3cdec4885e763688cc6089ae17a2c85466a
Lexi Blesius <git@blesius.eu>
2024-02-28 19:48:41 +0100

    Sort names alphabetically with script

---
+++ dev/sort_oreo_names.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python3
+
+# WARNING: running this script WILL override the input file!
+# Execute with great care!
+
+with open("src/data/oreo_names.txt") as f:
+    for line in f:
+        lines = [line.rstrip() for line in f]
+
+names = sorted([line for line in lines])
+
+with open("src/data/oreo_names.txt", 'w') as f:
+    for line in names:
+        f.write(line + '\n')
