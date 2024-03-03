1538d2507e70dc2cd5a69c9f8f30c3c471831ea9
Lexi Blesius <git@blesius.eu>
2024-03-03 21:51:58 +0100

    Move data out of src/
    This feels cleaner, to have data not be part of the source code but a
    separate part of the directory tree.

--- dev/sort_oreo_names.py
+++ dev/sort_oreo_names.py
@@ -3,12 +3,12 @@
 # WARNING: running this script WILL override the input file!
 # Execute with great care!
 
-with open("src/data/oreo_names.txt") as f:
+with open("data/oreo_names.txt") as f:
     for line in f:
         lines = [line.rstrip() for line in f]
 
 names = sorted([line for line in lines])
 
-with open("src/data/oreo_names.txt", 'w') as f:
+with open("data/oreo_names.txt", 'w') as f:
     for line in names:
         f.write(line + '\n')
