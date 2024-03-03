1956d12c3e47dc1c676a59c5c5bfae22287169b1
Lexi Blesius <git@blesius.eu>
2024-03-03 21:52:55 +0100

    Read names from file

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,24 @@
 use rand::seq::IteratorRandom;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 
 fn main() {
     let mut rng = rand::thread_rng();
     let faces = "😀😎😐😕😠😢";
     println!("I am {}!", faces.chars().choose(&mut rng).unwrap());
+
+    let names = names_array_from_file("data/oreo_names.txt");
+    for n in &names {
+        println!("{n}");
+    }
+}
+
+fn names_array_from_file(filepath: &str) -> Vec<String> {
+    let file = File::open(filepath).expect("could not open file!");
+    let buf_reader = BufReader::new(file);
+
+    buf_reader
+        .lines()
+        .map(|l| l.expect("Could not parse line"))
+        .collect()
 }
